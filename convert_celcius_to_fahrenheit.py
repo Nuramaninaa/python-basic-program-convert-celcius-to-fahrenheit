ca7e92ad6430b66d4b230f0c52a8a9a0a6c276ef
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-18 19:34:12 +0800

    Add files via upload

---
+++ convert_celcius_to_fahrenheit.py
@@ -0,0 +1,8 @@
+#python program to convert Celsius to Fahrenheit
+
+
+celcius = float(input("Enter the temperature of degree : "))
+
+fahrenheit = (celcius * 1.8) + 32
+
+print("Temperature of degree in celcius is ", fahrenheit) 
